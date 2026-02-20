1fc1b9aa0f6b9b71db464a18a7903f223915176b
Tran Phong <n.tranphongse@gmail.com>
2026-02-20 20:55:22 +0700

    [CFS-29 CFS-30 CFS-33] Add encryption using default ASP.NET Identity PasswordHasher

--- Confessly.Services/UserServices.cs
+++ Confessly.Services/UserServices.cs
@@ -3,22 +3,42 @@ using Confessly.Logging.Interfaces;
 using Confessly.Repository.Core;
 using Confessly.Services.Core;
 using Confessly.Services.Validation;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Confessly.Services
 {
-    public class UserServices(IUnitOfWork work, ILoggingService logger) : BaseServices(work, logger)
+    public class UserServices : BaseServices
     {
+        private readonly PasswordHasher<User> _passwordHasher;
+
+        public UserServices(IUnitOfWork work, ILoggingService logger) : base(work, logger)
+        {
+            _passwordHasher = new PasswordHasher<User>();
+        }
+
         public async Task<User> CreateUser(User user, CancellationToken cancellationToken)
         {
             user.Validate();
+            user.Password = HashPassword(user);
             var createdUser = await _work.Users.Insert(user, cancellationToken);
             await _work.SaveChanges(cancellationToken);
             createdUser.Password = string.Empty;
 
             return createdUser;
+        }
+
+        private string HashPassword(User user)
+        {
+            return _passwordHasher.HashPassword(user, user.Password);
+        }
+
+        private bool VerifyPassword(User user, string encryptedPassword)
+        {
+            var result = _passwordHasher.VerifyHashedPassword(user, user.Password, encryptedPassword);
+            return result == PasswordVerificationResult.Success;
         }
     }
 }
